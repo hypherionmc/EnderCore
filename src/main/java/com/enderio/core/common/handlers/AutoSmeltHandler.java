72d689a04851cc5723921cdc3aa1f362942ead36
OneLemonyBoi <50998649+OneLemonyBoi@users.noreply.github.com>
2021-04-27 14:34:11 -0700

    Fixes weird recipe deletion issue

--- src/main/java/com/enderio/core/common/handlers/AutoSmeltHandler.java
+++ src/main/java/com/enderio/core/common/handlers/AutoSmeltHandler.java
@@ -9,11 +9,14 @@ import net.minecraft.item.crafting.FurnaceRecipe;
 import net.minecraft.item.crafting.IRecipeType;
 import net.minecraft.loot.LootContext;
 import net.minecraft.loot.LootParameters;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 import java.util.List;
 import java.util.Random;
@@ -22,22 +25,23 @@ import java.util.Random;
 public class AutoSmeltHandler {
     @SubscribeEvent
     public static void handleBlockBreak(BreakEvent event) {
-        if (!event.getWorld().isRemote() && EnchantmentHelper.getEnchantmentLevel(EnchantmentAutoSmelt.instance(), event.getPlayer().getActiveItemStack()) > 0) { // Checks if running on server and enchant is on tool
+        if (!event.getWorld().isRemote() && EnchantmentHelper.getEnchantmentLevel(EnchantmentAutoSmelt.instance(), event.getPlayer().getHeldItem(Hand.MAIN_HAND)) > 0) { // Checks if running on server and enchant is on tool
             ServerWorld serverWorld = ((ServerWorld) event.getWorld()); // Casts IWorld to ServerWorld
             LootContext.Builder lootcontext$builder = (new LootContext.Builder(serverWorld)
-                    .withRandom(new Random())
+                    .withRandom(serverWorld.rand)
                     .withParameter(LootParameters.field_237457_g_, Vector3d.copyCentered(event.getPos()))
-                    .withParameter(LootParameters.TOOL, event.getPlayer()
-                            .getActiveItemStack())); // Makes lootcontext to calculate drops
+                    .withParameter(LootParameters.TOOL, event.getPlayer().getHeldItem(Hand.MAIN_HAND))); // Makes lootcontext to calculate drops
             List<ItemStack> drops = event.getState().getDrops(lootcontext$builder); // Calculates drops
-            for (ItemStack itemStack : drops) { // Iteration
-                if (serverWorld.getRecipeManager().getRecipe(IRecipeType.SMELTING, new Inventory(itemStack), serverWorld).isPresent()) { // Checks if recipe is present
-                    FurnaceRecipe recipe = serverWorld.getRecipeManager().getRecipe(IRecipeType.SMELTING, new Inventory(itemStack), serverWorld).get(); // Recipe as var
-                    InventoryHelper.spawnItemStack(serverWorld, event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), recipe.getRecipeOutput()); // Spawns Itemstack
-                }
+            for (ItemStack item : drops) { // Iteration
+                ItemStack stack = serverWorld.getRecipeManager().getRecipe(IRecipeType.SMELTING, new Inventory(item), serverWorld)
+                        .map(FurnaceRecipe::getRecipeOutput)
+                        .filter(itemStack -> !itemStack.isEmpty())
+                        .map(itemStack -> ItemHandlerHelper.copyStackWithSize(itemStack, item.getCount() * itemStack.getCount()))
+                        .orElse(item); // Recipe as var
+                InventoryHelper.spawnItemStack(event.getPlayer().world, event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), stack); // Spawns Itemstack
             }
-            serverWorld.destroyBlock(event.getPos(), false); // Breaks block
-            event.setCanceled(true); // Cancels the player breaking the block, shouldn't be required, but there just in case
+            event.getPlayer().world.destroyBlock(event.getPos(), false); // Breaks block
+            event.setResult(Event.Result.DENY);
         }
     }
 }
